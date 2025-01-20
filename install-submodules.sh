8c3b25d82f2206e8bfcbffd0f26327ddb4e32966
Lucas Manuel <lucasmanuel.tech@gmail.com>
2025-01-20 13:05:39 -0600

    ci: Fix private submodule issue (SC-925) (#7)
    * ci: try sub
    
    * ci: update yaml
    
    * ci: try updating shell script
    
    * ci: rm checkout step
    
    * ci: add checkout back
    
    * ci: fix ci

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,10 +1,18 @@
-mkdir $HOME/.ssh
-touch $HOME/.ssh/id_rsa
+# Create .ssh folder
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+
+# Add private key
+echo -e "$SSH_KEY_SPARK_ADDRESS_REGISTRY_PRIVATE" > $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
 
+# Add GitHub to known hosts
+ssh-keyscan github.com >> $HOME/.ssh/known_hosts
+
+# Configure git to use SSH instead of HTTPS
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 
-echo "$SSH_KEY_SPARK_ADDRESS_REGISTRY_PRIVATE" > $HOME/.ssh/id_rsa
+# Update submodules
 git submodule update --init --recursive lib/spark-address-registry
 
 forge install
