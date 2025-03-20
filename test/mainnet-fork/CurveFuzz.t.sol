6a24352001d9abbc2568bf55b0854f0e4141d2dc
Lucas Manuel <lucasmanuel.tech@gmail.com>
2025-03-20 07:51:14 -0600

    feat: working add liquidity

--- test/mainnet-fork/CurveFuzz.t.sol
+++ test/mainnet-fork/CurveFuzz.t.sol
@@ -7,6 +7,8 @@ import "./ForkTestBase.t.sol";
 
 import { ICurvePoolLike } from "../../src/MainnetController.sol";
 
+interface ICurvePool is IERC20, ICurvePoolLike {}
+
 contract ControllerHandler is Test {
 
     address immutable almProxy;
@@ -15,11 +17,14 @@ contract ControllerHandler is Test {
     IERC20 immutable token1;
     IERC20 immutable token2;
 
-    ICurvePoolLike    immutable pool;
+    ICurvePool        immutable pool;
     MainnetController immutable controller;
 
     uint256 immutable token1Precision;
     uint256 immutable token2Precision;
+
+    uint256 public totalValueDeposited;
+    uint256 public totalValueWithdrawn;
 
     constructor(
         address almProxy_,
@@ -38,13 +43,13 @@ contract ControllerHandler is Test {
         token1Precision = 10 ** token1.decimals();
         token2Precision = 10 ** token2.decimals();
 
-        pool       = ICurvePoolLike(pool_);
+        pool       = ICurvePool(pool_);
         controller = MainnetController(controller_);
     }
 
     function addLiquidity(uint256 token1Amount, uint256 token2Amount) public {
         token1Amount = _bound(token1Amount, 0, 100_000_000 * token1Precision);
-        token2Amount = _bound(token2Amount, 0, 100_000_000 * token1Precision);
+        token2Amount = _bound(token2Amount, 0, 100_000_000 * token2Precision);
 
         deal(address(token1), almProxy, token1Amount);
         deal(address(token2), almProxy, token2Amount);
@@ -55,8 +60,11 @@ contract ControllerHandler is Test {
 
         uint256[] memory rates = pool.stored_rates();
 
-        uint256 minLpAmount = (token1Amount * rates[0] + token2Amount * rates[1])
-            / 1e18
+        uint256 totalDeposit = (token1Amount * rates[0] + token2Amount * rates[1]) / 1e18;
+
+        totalValueDeposited += totalDeposit;
+
+        uint256 minLpAmount = totalDeposit
             * controller.maxSlippages(address(pool))
             / pool.get_virtual_price();
 
@@ -71,45 +79,51 @@ contract CurveFuzzTestsBase is ForkTestBase {
     address constant RLUSD      = 0x8292Bb45bf1Ee4d140127049757C2E0fF06317eD;
     address constant CURVE_POOL = 0xD001aE433f254283FeCE51d4ACcE8c53263aa186;
 
-    IERC20 rlUsd   = IERC20(RLUSD);
-    IERC20 curveLp = IERC20(CURVE_POOL);
+    IERC20 rlUsd = IERC20(RLUSD);
 
-    ICurvePoolLike curvePool = ICurvePoolLike(CURVE_POOL);
+    ICurvePool curvePool = ICurvePool(CURVE_POOL);
 
-    bytes32 curveDepositKey;
-    bytes32 curveSwapKey;
-    bytes32 curveWithdrawKey;
+    ControllerHandler handler;
 
     function setUp() public virtual override  {
         super.setUp();
 
-        curveDepositKey  = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_DEPOSIT(),  CURVE_POOL);
-        curveSwapKey     = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_SWAP(),     CURVE_POOL);
-        curveWithdrawKey = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_WITHDRAW(), CURVE_POOL);
+        bytes32 curveDepositKey  = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_DEPOSIT(),  CURVE_POOL);
+        bytes32 curveSwapKey     = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_SWAP(),     CURVE_POOL);
+        bytes32 curveWithdrawKey = RateLimitHelpers.makeAssetKey(mainnetController.LIMIT_CURVE_WITHDRAW(), CURVE_POOL);
 
         vm.startPrank(SPARK_PROXY);
-        rateLimits.setRateLimitData(curveDepositKey,  2_000_000e18, uint256(2_000_000e18) / 1 days);
-        rateLimits.setRateLimitData(curveSwapKey,     1_000_000e18, uint256(1_000_000e18) / 1 days);
-        rateLimits.setRateLimitData(curveWithdrawKey, 3_000_000e18, uint256(3_000_000e18) / 1 days);
+        rateLimits.setUnlimitedRateLimitData(curveDepositKey);
+        rateLimits.setUnlimitedRateLimitData(curveSwapKey);
+        rateLimits.setUnlimitedRateLimitData(curveWithdrawKey);
         vm.stopPrank();
 
         // Set a higher slippage to allow for successes
         vm.prank(SPARK_PROXY);
-        mainnetController.setMaxSlippage(CURVE_POOL, 0.98e18);
+        mainnetController.setMaxSlippage(CURVE_POOL, 0.99e18);
 
-        ControllerHandler handler = new ControllerHandler(
+        handler = new ControllerHandler(
             address(almProxy),
             address(mainnetController),
             CURVE_POOL,
             relayer,
-            RLUSD,
-            address(usdc)
+            address(usdc),
+            RLUSD
         );
+
+        targetContract(address(handler));
+    }
+
+    function _getBlock() internal pure override returns (uint256) {
+        return 22000000;  // March 8, 2025
     }
 
     function statefulFuzz_curve_test() public {
-        // assertEq(rlUsd.balanceOf(address(this)), 0);
+        uint256 lpValue = curvePool.balanceOf(address(almProxy)) * curvePool.get_virtual_price() / 1e18;
+
+        assertGe(lpValue, handler.totalValueDeposited() * 99/100);
     }
+
 }
 
 
