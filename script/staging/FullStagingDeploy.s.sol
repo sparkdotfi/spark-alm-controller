c151d810cb77cce97e8cb8fb916bab8b3820ac0e
Lucas Manuel <lucasmanuel.tech@gmail.com>
2025-04-02 06:54:29 -0600

    fix: cleanup

--- script/staging/FullStagingDeploy.s.sol
+++ script/staging/FullStagingDeploy.s.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-import { console } from "forge-std/console.sol";
-
 import {
     AllocatorDeploy,
     AllocatorIlkInstance,
