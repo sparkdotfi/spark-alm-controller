f8122ac00b15422f939c147b55913312fe05ae7b
Lucas Manuel <lucasmanuel.tech@gmail.com>
2025-10-08 12:07:20 -0400

    feat: Update OTC swap functionality (SC-1116) (#156)
    * M Feat: Add `offchainSwap` and Buffer.sol.
    
    * M Feat: First draft of Offchain Swap Module.
    
    * M Feat: First draft of Offchain Swap Module.
    
    * M Feat: First draft of Offchain Swap Module.
    
    * S Feat: First draft of Offchain Swap Module.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * M Feat: Extract Otc structs.
    
    * S Fix: Incorrect expression in last commit
    
    * S Feat: Small changes.
    
    * S Feat: Doubly linked exchanges and otcBuffers.
    
    * S Feat: Remove doubly linked exchanges and otcBuffers.
    
    * S Feat: Small changes
    
    * S Feat: Small changes
    
    * S Feat: Small changes
    
    * test: add OTCSwaps test suite for mainnet fork testing
    
    * Backup
    
    * Backup
    
    * Backup
    
    * Backup
    
    * Backup
    
    * Backup
    
    * M Feat: OTC swap: Full success test case.
    
    * Backup
    
    * Backup
    
    * Backup
    
    * M Feat: Finish test of OTC swap.
    
    * S Feat: Rename OTCBuffer.sol
    
    * S Feat: Rename _OTCBuffer.sol
    
    * Backup
    
    * Backup
    
    * Backup
    
    * L Feat: Add more tests.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * Backup
    
    * L Feat: Use SafeERC20 in OTCBuffer; rearchitect tests to use usdt and usdc.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Small changes.
    
    * S Feat: Before major refactor.
    
    * Backup
    
    * Backup
    
    * M Feat: Refactor otcClaim tests to only four cases.
    
    * Backup
    
    * S Feat: Small changes
    
    * S Feat: Before refactor to getClaimedWithRecharge
    
    * S Feat: Add getClaimedWithRecharge alongside isOtcSwapReady.
    
    * S Feat: Add tests for getClaimedWithRecharge.
    
    * S Feat: Small changes.
    
    * fix: first round of changes
    
    * fix: update more tests
    
    * feat: add success tests
    
    * feat: refactor to use claimed timestamp
    
    * feat: udpate to add helper function
    
    * fix: rm lib files
    
    * feat: e2e tests passing
    
    * feat: recharge tests working
    
    * fix: e2e tests working again
    
    * feat: add final tests
    
    * feat: add otc buffer tests
    
    * fix: imports
    
    * fix: align
    
    ---------
    
    Co-authored-by: Hacker-Dom <18601956+hacker-DOM@users.noreply.github.com>

---
+++ test/unit/OTCBuffer.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.21;
+
+import { ERC20Mock } from "openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
+
+import { OTCBuffer } from "src/OTCBuffer.sol";
+
+import { UnitTestBase } from "./UnitTestBase.t.sol";
+
+contract OTCBufferTestBase is UnitTestBase {
+
+    OTCBuffer public buffer;
+    ERC20Mock public usdt;
+
+    function setUp() public {
+        buffer = new OTCBuffer(admin);
+        usdt   = new ERC20Mock();
+    }
+
+}
+
+contract OTCBufferConstructorTest is OTCBufferTestBase {
+
+    function test_constructor() public {
+        assertEq(buffer.hasRole(DEFAULT_ADMIN_ROLE, admin), true);
+
+        address newAdmin = makeAddr("new-admin");
+
+        OTCBuffer newBuffer = new OTCBuffer(newAdmin);
+
+        assertEq(newBuffer.hasRole(DEFAULT_ADMIN_ROLE, newAdmin), true);
+    }
+
+}
+
+
+contract OTCBufferApproveFailureTests is OTCBufferTestBase {
+
+    function test_approve_notAuthorized() public {
+        vm.expectRevert(abi.encodeWithSignature(
+            "AccessControlUnauthorizedAccount(address,bytes32)",
+            address(this),
+            DEFAULT_ADMIN_ROLE
+        ));
+        buffer.approve(address(usdt), address(buffer), 1_000_000e6);
+    }
+
+}
+
+contract OTCBufferApproveSuccessTests is OTCBufferTestBase {
+
+    function test_approve() public {
+        assertEq(usdt.allowance(address(buffer), address(buffer)), 0);
+
+        vm.prank(admin);
+        buffer.approve(address(usdt), address(buffer), 1_000_000e6);
+
+        assertEq(usdt.allowance(address(buffer), address(buffer)), 1_000_000e6);
+    }
+
+}
+
+contract OTCBufferReceiveEthTests is OTCBufferTestBase {
+
+    function test_receiveEth() public {
+        address user = makeAddr("user");
+
+        deal(user, 10 ether);
+
+        assertEq(address(user).balance,   10 ether);
+        assertEq(address(buffer).balance, 0);
+
+        vm.prank(user);
+        payable(address(buffer)).transfer(10 ether);
+
+        assertEq(address(user).balance,   0);
+        assertEq(address(buffer).balance, 10 ether);
+    }
+
+}
