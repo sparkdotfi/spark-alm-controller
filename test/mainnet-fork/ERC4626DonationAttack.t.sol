80741c159887529dc492a3280ae1d538eee212d8
Hacker-Dom <18601956+hacker-DOM@users.noreply.github.com>
2025-10-14 15:01:22 -0400

    Backup: Passing tests, setup complete.

--- test/mainnet-fork/ERC4626DonationAttack.t.sol
+++ test/mainnet-fork/ERC4626DonationAttack.t.sol
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.8.0;
 
-// import { IERC4626 } from "openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
+import { IMetaMorpho, Id } from "metamorpho/interfaces/IMetaMorpho.sol";
+
+import { MarketParamsLib }       from "morpho-blue/src/libraries/MarketParamsLib.sol";
+import { IMorpho, MarketParams } from "morpho-blue/src/interfaces/IMorpho.sol";
 
 import "./ForkTestBase.t.sol";
 
 contract ERC4626DonationAttackTestBase is ForkTestBase {
 
-    address morpho_vault = IERC4626(0xe41a0583334f0dc4E023Acd0bFef3667F6FE0597);
+    IMetaMorpho morpho_vault = IMetaMorpho(0xe41a0583334f0dc4E023Acd0bFef3667F6FE0597);
+
+    address curator        = makeAddr("curator");
+    address guardian       = makeAddr("guardian");
+    address fee_recipient  = makeAddr("fee_recipient");
+    address allocator      = makeAddr("allocator");
+    address skim_recipient = makeAddr("skim_recipient");
 
     function setUp() override public {
         super.setUp();
@@ -16,17 +25,42 @@ contract ERC4626DonationAttackTestBase is ForkTestBase {
         bytes32 withdrawKey = RateLimitHelpers.makeAddressKey(mainnetController.LIMIT_4626_WITHDRAW(), address(morpho_vault));
 
         // Basic validation
-        bytes memory data = abi.encodeWithSignature("symbol()");
-        (, bytes memory returnData) = morpho_vault.call(data);
-        assertEq(keccak256(returnData), keccak256(abi.encode("sparkUSDS")));
+        assertEq(keccak256(abi.encode(morpho_vault.symbol())), keccak256(abi.encode("sparkUSDS")));
+        assertEq(morpho_vault.totalAssets(), 0);
+        assertEq(morpho_vault.totalSupply(), 0);
 
-        data = abi.encodeWithSignature("totalAssets()");
-        (, returnData) = morpho_vault.call(data);
-        assertEq(abi.decode(returnData, (uint256)), 0);
+        // Initialization
+        vm.startPrank(Ethereum.SPARK_PROXY);
+        morpho_vault.setCurator(curator);
+        morpho_vault.submitGuardian(guardian);
+        morpho_vault.setFeeRecipient(fee_recipient);
+        morpho_vault.setIsAllocator(allocator, true);
+        morpho_vault.setSkimRecipient(skim_recipient);
 
-        data = abi.encodeWithSignature("totalSupply()");
-        (, returnData) = morpho_vault.call(data);
-        assertEq(abi.decode(returnData, (uint256)), 0);
+        // Choose the Morpho market you want to allocate to
+        MarketParams memory marketParams = MarketParams({
+            loanToken: Ethereum.USDS,
+            collateralToken: address(0x0),
+            oracle: address(0x0),
+            irm: address(0x0),
+            lltv: 0
+        });
+        Id marketId = MarketParamsLib.id(marketParams);
+
+        morpho_vault.submitCap(marketParams, 10_000_000e18);
+        skip(morpho_vault.timelock());  // Wait the timelock
+        morpho_vault.acceptCap(marketParams);
+
+        // Now that the market has a non-zero cap, set the supply queue order
+        Id[] memory supplyOrder = new Id[](1);
+        supplyOrder[0] = marketId;
+        morpho_vault.setSupplyQueue(supplyOrder);
+        vm.stopPrank();
+
+        assertEq(morpho_vault.curator(), curator);
+        assertEq(morpho_vault.guardian(), guardian);
+        assertEq(morpho_vault.feeRecipient(), fee_recipient);
+        assertTrue(morpho_vault.isAllocator(allocator));
 
         vm.startPrank(Ethereum.SPARK_PROXY);
         rateLimits.setRateLimitData(depositKey,  5_000_000e18, uint256(1_000_000e18) / 4 hours);
@@ -49,9 +83,9 @@ contract ERC4626DonationAttack is ERC4626DonationAttackTestBase {
         deal(address(usds), mallory, 10);
 
         vm.startPrank(mallory);
-        usds.approve(morpho_vault, 1);
-        // morpho_vault.call(abi.encodeWithSignature("deposit(uint256,address)", 1, mallory));
-        usds.transfer(morpho_vault, 9);
+        usds.approve(address(morpho_vault), 1);
+        morpho_vault.deposit(1, mallory);
+        usds.transfer(address(morpho_vault), 9);
         vm.stopPrank();
 
         // assertEq()
