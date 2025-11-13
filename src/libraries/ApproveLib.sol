1754ec8d5123a891a82e4e12a746bde9aa475c0c
supercontracts <bonjour.tabish@gmail.com>
2025-11-13 20:21:33 +0530

    fix: Use relative paths (#191)

--- src/libraries/ApproveLib.sol
+++ src/libraries/ApproveLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 import { IALMProxy } from "../interfaces/IALMProxy.sol";
 
