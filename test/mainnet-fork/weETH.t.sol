de80f1b928c072bc6ae57751533faf1aa8bd0528
Tabish Shaikh <bonjour.tabish@gmail.com>
2026-01-29 18:32:40 +0530

    fix

--- test/mainnet-fork/weETH.t.sol
+++ test/mainnet-fork/weETH.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import { ILiquidityPoolLike, IEETHLike } from "../../src/libraries/WEETHLib.sol";
+import { IEETHLike } from "../../src/libraries/WEETHLib.sol";
 
 import { WEETHModule } from "../../src/WEETHModule.sol";
 
