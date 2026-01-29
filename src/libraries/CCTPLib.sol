0f6cfe779aa695ca36d17a5b929c2ee4c9162d12
Michael De Luca <michael.deluca@circle-free.com>
2026-01-29 13:39:32 -0500

    refactor: cleaner CCTPLib

--- src/libraries/CCTPLib.sol
+++ src/libraries/CCTPLib.sol
@@ -31,12 +31,12 @@ library CCTPLib {
     /*** Events                                                                                 ***/
     /**********************************************************************************************/
 
-    // NOTE: This is used to track individual transfers for offchain processing of CCTP transactions
+    // NOTE: Used to track individual transfers for off-chain processing of CCTP transactions.
     event CCTPTransferInitiated(
         uint64  indexed nonce,
         uint32  indexed destinationDomain,
         bytes32 indexed mintRecipient,
-        uint256 usdcAmount
+        uint256         usdcAmount
     );
 
     /**********************************************************************************************/
@@ -45,6 +45,7 @@ library CCTPLib {
 
     function transferUSDCToCCTP(TransferUSDCToCCTPParams calldata params) external {
         _rateLimited(params.rateLimits, params.cctpRateLimitId, params.usdcAmount);
+
         _rateLimited(
             params.rateLimits,
             RateLimitHelpers.makeUint32Key(params.domainRateLimitId, params.destinationDomain),
@@ -53,37 +54,30 @@ library CCTPLib {
 
         require(params.mintRecipient != 0, "MC/domain-not-configured");
 
-        // Approve USDC to CCTP from the proxy (assumes the proxy has enough USDC)
+        // Approve USDC to CCTP from the proxy (assumes the proxy has enough USDC).
         _approve(params.proxy, address(params.usdc), address(params.cctp), params.usdcAmount);
 
-        // If amount is larger than limit it must be split into multiple calls
+        // If amount is larger than limit it must be split into multiple calls.
         uint256 burnLimit = params.cctp.localMinter().burnLimitsPerMessage(address(params.usdc));
 
-        // This variable will get reduced in the loop below
+        require(burnLimit > 0, "MC/burn-limit-not-set");
+
+        // This variable will get reduced in the loop below.
         uint256 usdcAmountTemp = params.usdcAmount;
 
-        while (usdcAmountTemp > burnLimit) {
-            _initiateCCTPTransfer(
-                params.proxy,
-                params.cctp,
-                params.usdc,
-                burnLimit,
-                params.mintRecipient,
-                params.destinationDomain
-            );
-            usdcAmountTemp -= burnLimit;
-        }
+        while (usdcAmountTemp > 0) {
+            uint256 amount = usdcAmountTemp > burnLimit ? burnLimit : usdcAmountTemp;
 
-        // Send remaining amount (if any)
-        if (usdcAmountTemp > 0) {
             _initiateCCTPTransfer(
                 params.proxy,
                 params.cctp,
                 params.usdc,
-                usdcAmountTemp,
+                amount,
                 params.mintRecipient,
                 params.destinationDomain
             );
+
+            usdcAmountTemp -= amount;
         }
     }
 
@@ -91,7 +85,7 @@ library CCTPLib {
     /*** Relayer helper functions                                                               ***/
     /**********************************************************************************************/
 
-    // NOTE: As USDC is the only asset transferred using CCTP, _forceApprove logic is unnecessary.
+    // NOTE: As USDC is the only asset transferred using CCTP, `_forceApprove` logic is unnecessary.
     function _approve(
         IALMProxy proxy,
         address   token,
