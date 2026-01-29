28fa2e5b7808feace5b3c54644e76de4bb95ab59
Tabish Shaikh <bonjour.tabish@gmail.com>
2026-01-29 21:18:03 +0530

    add struct

--- src/OTCBuffer.sol
+++ src/OTCBuffer.sol
@@ -13,7 +13,24 @@ contract OTCBuffer is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
 
     using SafeERC20 for IERC20;
 
-    address public almProxy;
+    /**********************************************************************************************/
+    /*** UUPS Storage                                                                           ***/
+    /**********************************************************************************************/
+
+    struct BufferStorage {
+        address almProxy;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("otcBuffer.storage.Buffer")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 internal constant _BUFFER_STORAGE_LOCATION =
+        0xa8ff6143ce9b2840ac86932ea3c593f97be7f3f4c76899ca3e385ef6d4c71f00;
+
+    function _getBufferStorage() internal pure returns (BufferStorage storage $) {
+        // slither-disable-next-line assembly
+        assembly {
+            $.slot := _BUFFER_STORAGE_LOCATION
+        }
+    }
 
     /**********************************************************************************************/
     /*** Initialization                                                                         ***/
@@ -32,7 +49,9 @@ contract OTCBuffer is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
 
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
 
-        almProxy = almProxy_;
+        BufferStorage storage $ = _getBufferStorage();
+
+        $.almProxy = almProxy_;
     }
 
     // Only DEFAULT_ADMIN_ROLE can upgrade the implementation
@@ -45,7 +64,9 @@ contract OTCBuffer is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
     function approve(address asset, uint256 allowance)
         external onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        IERC20(asset).forceApprove(almProxy, allowance);
+        BufferStorage storage $ = _getBufferStorage();
+
+        IERC20(asset).forceApprove($.almProxy, allowance);
     }
 
 }
