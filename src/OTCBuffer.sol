8f97905737037437bcf0457ba37337608a73f1c6
supercontracts <bonjour.tabish@gmail.com>
2026-01-29 22:29:26 +0530

    feat: Make OTCBuffer Upgradeable (DEV-1158) (#232)
    * save
    
    * fix: review
    
    * fix: review
    
    * add struct
    
    * fix: review
    
    * fix: review
    
    ---------
    
    Co-authored-by: Lucas Manuel <lucasmanuel.tech@gmail.com>

--- src/OTCBuffer.sol
+++ src/OTCBuffer.sol
@@ -1,27 +1,58 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-import { AccessControlEnumerable }  from "openzeppelin-contracts/contracts/access/extensions/AccessControlEnumerable.sol";
 import { IERC20Metadata as IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 }                from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract OTCBuffer is AccessControlEnumerable {
+import { UUPSUpgradeable } from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+import { AccessControlEnumerableUpgradeable } 
+    from "openzeppelin-contracts-upgradeable/contracts/access/extensions/AccessControlEnumerableUpgradeable.sol";
+
+contract OTCBuffer is AccessControlEnumerableUpgradeable, UUPSUpgradeable {
 
     using SafeERC20 for IERC20;
 
-    address public immutable almProxy;
+    /**********************************************************************************************/
+    /*** UUPS Storage                                                                           ***/
+    /**********************************************************************************************/
+
+    struct OTCBufferStorage {
+        address almProxy;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("almController.storage.OTCBuffer")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 internal constant _OTC_BUFFER_STORAGE_LOCATION =
+        0xe0e561841bb6fa9b0b4be53b5b4f5d506ea40664f6db7ecbcf7b6f18935a4f00;
+
+    function _getOTCBufferStorage() internal pure returns (OTCBufferStorage storage $) {
+        assembly {
+            $.slot := _OTC_BUFFER_STORAGE_LOCATION
+        }
+    }
 
     /**********************************************************************************************/
     /*** Initialization                                                                         ***/
     /**********************************************************************************************/
 
-    constructor(address admin, address _almProxy) {
-        require(_almProxy != address(0), "OTCBuffer/invalid-alm-proxy");
+    constructor() {
+        _disableInitializers();  // Avoid initializing in the context of the implementation
+    }
+
+    function initialize(address admin, address almProxy_) external initializer {
+        require(admin     != address(0), "OTCBuffer/invalid-admin");
+        require(almProxy_ != address(0), "OTCBuffer/invalid-alm-proxy");
+
+        __AccessControlEnumerable_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
 
-        almProxy = _almProxy;
+        _getOTCBufferStorage().almProxy = almProxy_;
     }
+
+    // Only DEFAULT_ADMIN_ROLE can upgrade the implementation
+    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
     /**********************************************************************************************/
     /*** Call functions                                                                         ***/
@@ -30,7 +61,11 @@ contract OTCBuffer is AccessControlEnumerable {
     function approve(address asset, uint256 allowance)
         external onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        IERC20(asset).forceApprove(almProxy, allowance);
+        IERC20(asset).forceApprove(almProxy(), allowance);
+    }
+
+    function almProxy() public view returns (address) {
+        return _getOTCBufferStorage().almProxy;
     }
 
 }
